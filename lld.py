66b3638b26bec837d2fcaf73563643e0856e7b21
Milad Nekofar <milad@nekofar.com>
2019-06-29 02:44:28 +0430

    Add separated smaller methods for download course data

--- lld.py
+++ lld.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+"""
+A scraping tool that downloads video lessons from Linkedin Learning
+"""
 
 import urllib
 import sys
@@ -14,30 +17,30 @@ import config
 
 reload(sys)
 
-login_url = "https://www.linkedin.com/login"
-post_login_url = "https://www.linkedin.com/uas/login-submit"
-course_api_url = (
-    "https://www.linkedin.com/learning-api/detailedCourses"
-    "??fields=fullCourseUnlocked,releasedOn,"
-    "exerciseFileUrls,exerciseFiles&addParagraphsToTranscript=true&courseSlug={}&q=slugs"
+
+BASE_URL = "https://www.linkedin.com"
+
+LOGIN_URL = "{}/login"
+POST_LOGIN_URL = "{}/uas/login-submit"
+COURSE_API_URL = (
+    "{}/learning-api/detailedCourses?fields=fullCourseUnlocked,releasedOn,exerciseFileUrls,exerciseFiles"
+    "&addParagraphsToTranscript=true&courseSlug={}&q=slugs"
 )
-video_api_url = (
-    "https://www.linkedin.com/learning-api/detailedCourses"
-    "?addParagraphsToTranscript=false&courseSlug={}"
-    "&q=slugs&resolution=_720&videoSlug={}"
+VIDEO_API_URL = (
+    "{}/learning-api/detailedCourses?addParagraphsToTranscript=false"
+    "&courseSlug={}&q=slugs&resolution=_720&videoSlug={}"
 )
-headers = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,"
-    "image/webp,image/apng,*/*;q=0.8",
+HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "en-US,en;q=0.9",
     "Connection": "keep-alive",
     "Content-Type": "application/x-www-form-urlencoded",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
-    " (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36(KHTML, like Gecko)  "
+    "Chrome/66.0.3359.181 Safari/537.36",
 }
-
-colors = {
+COLORS = {
     "black": "\033[30m",
     "red": "\033[31m",
     "green": "\033[32m",
@@ -51,7 +54,11 @@ colors = {
 }
 
 
-class Lld(object):
+class LLD(object):
+    """
+
+    """
+
     def __init__(self):
         self.session = Session()
         self.base_path = (
@@ -98,13 +105,13 @@ class Lld(object):
         return raw_string[i:]
 
     @staticmethod
-    def format_time(ms):
+    def format_time(mill):
         """
 
-        :param ms:
+        :param mill:
         :return:
         """
-        seconds, milliseconds = divmod(ms, 1000)
+        seconds, milliseconds = divmod(mill, 1000)
         minutes, seconds = divmod(seconds, 60)
         hours, minutes = divmod(minutes, 60)
         return "{:d}:{:2d}:{:2d},{:2d}".format(hours, minutes, seconds, milliseconds)
@@ -117,11 +124,11 @@ class Lld(object):
         :param color:
         :param data:
         """
-        print "[{}] {}{}{}".format(
+        print "[{}]{}{}{}".format(
             datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            colors[color],
+            COLORS[color],
             str(data),
-            colors["default"],
+            COLORS["default"],
         )
 
     def download_file(self, url, path, file_name):
@@ -146,13 +153,13 @@ class Lld(object):
                 with tqdm(
                     desc="[{}]{}[*] {} {} {:0.2f}Mb".format(
                         datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                        colors[color],
+                        COLORS[color],
                         indent,
                         "Downloading",
                         total / 1e6,
                     ),
                     bar_format="{desc}: {percentage:2.0f}% | {elapsed}, {rate_fmt}"
-                    + colors["default"],
+                               + COLORS["default"],
                     total=total,
                     ncols=100,
                     leave=False,
@@ -170,6 +177,7 @@ class Lld(object):
 
     def download_sub(self, subs, path, file_name):
         """
+        Download course videos subtitles
 
         :param subs:
         :param path:
@@ -196,6 +204,7 @@ class Lld(object):
     @staticmethod
     def download_desc(desc, course_url, path, file_name):
         """
+        Download course description
 
         :param desc:
         :param course_url:
@@ -212,7 +221,10 @@ class Lld(object):
         Login to the LinkedIn using login data and initialize session
         """
         self.print_log("cyan", "[*] Authenticating to LinkedIn")
-        login_page = BeautifulSoup(self.session.get(login_url).text, "html.parser")
+        login_page = BeautifulSoup(
+            self.session.get(LOGIN_URL.format(BASE_URL)).text,
+            "html.parser",
+        )
         csrf = login_page.find("input", {"name": "loginCsrfParam"})["value"]
         self.print_log("cyan", "[*] Csfr token: {}".format(csrf))
         login_data = urllib.urlencode(
@@ -223,11 +235,15 @@ class Lld(object):
                 "loginCsrfParam": csrf,
             }
         )
-        headers["Cookie"] = self.plain_cookies(
+        HEADERS["Cookie"] = self.plain_cookies(
             requests.utils.dict_from_cookiejar(self.session.cookies)
         )
-        self.session.headers.update(headers)
-        resp = self.session.post(post_login_url, data=login_data, allow_redirects=True)
+        self.session.headers.update(HEADERS)
+        resp = self.session.post(
+            POST_LOGIN_URL.format(BASE_URL),
+            data=login_data,
+            allow_redirects=True,
+        )
         if resp.status_code != 200:
             self.print_log("red", "[!] Could not authenticate to LinkedIn")
         else:
@@ -245,128 +261,146 @@ class Lld(object):
         self.session.headers.pop("Accept")
 
         for course in config.COURSES:
-            resp = self.session.get(course_api_url.format(course))
-            course_data = resp.json()["elements"][0]
-            course_name = self.format_string(course_data["title"])
-            self.print_log(
-                "yellow", "[*] Starting download of course [{}]...".format(course_name)
-            )
-            course_path = "{}/{}".format(self.base_path, course_name)
-            chapters_list = course_data["chapters"]
-            chapter_index = 1
-            self.print_log("yellow", "[*] Parsing course's chapters...")
-            self.print_log(
-                "yellow", "[*] {:d} chapters found".format(len(chapters_list))
-            )
-            for chapter in chapters_list:
-                chapter_name = self.format_string(chapter["title"])
-                self.print_log(
-                    "green",
-                    "[*] --- Starting download of chapter [{}]...".format(chapter_name),
-                )
-                chapter_path = "{}/{} - {}".format(
-                    course_path, str(chapter_index).zfill(2), chapter_name
-                )
-                if chapter_name == "":
-                    chapter_path = chapter_path[:-3]
-                videos_list = chapter["videos"]
-                video_index = 1
-                self.print_log("green", "[*] --- Parsing chapters's videos")
-                self.print_log(
-                    "green", "[*] --- {:d} videos found".format(len(videos_list))
-                )
-                for video in videos_list:
-                    video_name = self.format_string(video["title"])
-                    video_slug = video["slug"]
-                    video_path = (
-                        chapter_path
-                        + "/"
-                        + "{} - {}.mp4".format(str(video_index).zfill(2), video_name)
-                    )
-                    if os.path.exists(video_path):
-                        self.print_log(
-                            "blue",
-                            "[*] ------ Skip video [{}] download "
-                            "because it already exists.".format(video_name),
-                        )
-                        video_index += 1
-                        continue
-                    video_data = self.session.get(
-                        video_api_url.format(course, video_slug)
-                    )
-                    try:
-                        video_url = re.search(
-                            '"progressiveUrl":"(.+)","streamingUrl"', video_data.text
-                        ).group(1)
-                    except AttributeError:
-                        self.print_log(
-                            "red",
-                            "[!] ------ Can't download the video [{}], "
-                            "probably is only for premium users".format(video_name),
-                        )
-                        continue
+            self.download_course(course)
+
+    def download_course(self, course):
+        """
+        Download an individual course
+        :param course:
+        """
+        resp = self.session.get(COURSE_API_URL.format(BASE_URL, course))
+        course_data = resp.json()["elements"][0]
+        course_name = self.format_string(course_data["title"])
+        self.print_log(
+            "yellow", "[*] Starting download of course [{}]...".format(course_name)
+        )
+        course_path = "{}/{}".format(self.base_path, course_name)
+        chapters_list = course_data["chapters"]
+        chapter_index = 1
+        self.print_log("yellow", "[*] Parsing course's chapters...")
+        self.print_log("yellow", "[*] {:d} chapters found".format(len(chapters_list)))
+        for chapter in chapters_list:
+            self.download_chapter(course, chapter, course_path, chapter_index)
+            chapter_index += 1
+
+        exercises_list = course_data["exerciseFiles"]
+        if exercises_list:
+            self.print_log("green", "[*] --- Downloading exercise files")
+            for exercise in exercises_list:
+                try:
+                    ex_name = exercise["name"]
+                    ex_url = exercise["url"]
+                except (KeyError, IndexError):
                     self.print_log(
-                        "magenta",
-                        "[*] ------ Downloading video [{}]".format(video_name),
+                        "default",
+                        "[!] --- Can't download an exercise file "
+                        "for course [{}]".format(course_name),
                     )
-                    self.download_file(
-                        video_url,
-                        chapter_path,
-                        "{} - {}.mp4".format(str(video_index).zfill(2), video_name),
-                    )
-                    video_data = video_data.json()["elements"][0]
-                    if config.SUBS:
-                        try:
-                            subs = video_data["selectedVideo"]["transcript"]["lines"]
-                        except KeyError:
-                            self.print_log("gray", "[*] ------ No subtitles available")
-                        else:
-                            self.print_log(
-                                "magenta", "[*] ------ Downloading subtitles"
-                            )
-                            self.download_sub(
-                                subs,
-                                chapter_path,
-                                "{} - {}.srt".format(
-                                    str(video_index).zfill(2), video_name
-                                ),
-                            )
-                    video_index += 1
-                chapter_index += 1
+                else:
+                    self.download_file(ex_url, course_path, ex_name)
+        else:
+            self.print_log("gray", "[*] --- No exercise files available")
 
-            exercises_list = course_data["exerciseFiles"]
-            if exercises_list:
-                self.print_log("green", "[*] --- Downloading exercise files")
-                for exercise in exercises_list:
-                    try:
-                        ex_name = exercise["name"]
-                        ex_url = exercise["url"]
-                    except (KeyError, IndexError):
-                        self.print_log(
-                            "default",
-                            "[!] --- Can't download an exercise file "
-                            "for course [{}]".format(course_name),
-                        )
-                    else:
-                        self.download_file(ex_url, course_path, ex_name)
-            else:
-                self.print_log("gray", "[*] --- No exercise files available")
+        description = course_data["description"]
+        self.print_log("green", "[*] --- Downloading course description")
+        self.download_desc(
+            description,
+            "https://www.linkedin.com/learning/{}".format(course),
+            course_path,
+            "Description.txt",
+        )
 
-            description = course_data["description"]
-            self.print_log("green", "[*] --- Downloading course description")
-            self.download_desc(
-                description,
-                "https://www.linkedin.com/learning/{}".format(course),
-                course_path,
-                "Description.txt",
+    def download_chapter(self, course, chapter, course_path, chapter_index):
+        """
+        Download course chapter and videos
+        :param course:
+        :param chapter:
+        :param course_path:
+        :param chapter_index:
+        """
+        chapter_name = self.format_string(chapter["title"])
+        self.print_log(
+            "green", "[*] --- Starting download of chapter [{}]...".format(chapter_name)
+        )
+        chapter_path = "{}/{} - {}".format(
+            course_path, str(chapter_index).zfill(2), chapter_name
+        )
+        if chapter_name == "":
+            chapter_path = chapter_path[:-3]
+        videos_list = chapter["videos"]
+        video_index = 1
+        self.print_log("green", "[*] --- Parsing chapters's videos")
+        self.print_log("green", "[*] --- {:d} videos found".format(len(videos_list)))
+        for video in videos_list:
+            self.download_video(course, video, chapter_path, video_index)
+            video_index += 1
+
+    def download_video(self, course, video, chapter_path, video_index):
+        """
+        Download course video
+
+        :param course:
+        :param video:
+        :param chapter_path:
+        :param video_index:
+        :return:
+        """
+        video_name = self.format_string(video["title"])
+        video_slug = video["slug"]
+        video_path = (
+            chapter_path
+            + "/"
+            + "{} - {}.mp4".format(str(video_index).zfill(2), video_name)
+        )
+        if os.path.exists(video_path):
+            self.print_log(
+                "blue",
+                "[*] ------ Skip video [{}] download "
+                "because it already exists.".format(video_name),
             )
+            return
+        video_data = self.session.get(
+            VIDEO_API_URL.format(BASE_URL, course, video_slug)
+        )
+        try:
+            video_url = re.search(
+                '"progressiveUrl":"(.+)","streamingUrl"', video_data.text
+            ).group(1)
+        except AttributeError:
+            self.print_log(
+                "red",
+                "[!] ------ Can't download the video [{}], "
+                "probably is only for premium users".format(video_name),
+            )
+            return
+        self.print_log(
+            "magenta", "[*] ------ Downloading video [{}]".format(video_name)
+        )
+        self.download_file(
+            video_url,
+            chapter_path,
+            "{} - {}.mp4".format(str(video_index).zfill(2), video_name),
+        )
+        video_data = video_data.json()["elements"][0]
+        if config.SUBS:
+            try:
+                subs = video_data["selectedVideo"]["transcript"]["lines"]
+            except KeyError:
+                self.print_log("gray", "[*] ------ No subtitles available")
+            else:
+                self.print_log("magenta", "[*] ------ Downloading subtitles")
+                self.download_sub(
+                    subs,
+                    chapter_path,
+                    "{} - {}.srt".format(str(video_index).zfill(2), video_name),
+                )
 
 
 def main():
     """
 
     """
-    lld = Lld()
+    lld = LLD()
     lld.get_logged_session()
     lld.download_courses()
 
